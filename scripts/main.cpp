dc4440351db979ad11b64f7c148515f19fd26e02
QrJuicy <qrjuicy@gmail.com>
2023-12-01 19:32:29 +0800

    File input done

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -17,7 +17,7 @@ class Main
 {
 private:
 public: 
-    int combinations[216];
+    string combinations[216];
     int indexMPN[216];
     int lower95[216];
     int upper95[216];
@@ -28,14 +28,33 @@ int main()
 { 
     //cout << namespace_exit::x + namespace_insert::x;
     //cout << combi
-    inputFile.open("table1.txt", ios::in);
-    while (!inputFile.eof())
+
+    Main tableData;
+    ifstream inFile;
+    inFile.open("table1.txt");
+    int i = 0;
+    // inputFile.open("table1.txt", ios::in);
+    while (inFile >> 
+    tableData.combinations[i] >> 
+    tableData.indexMPN[i] >>
+    tableData.lower95[i] >>
+    tableData.upper95[i]
+    )
     {
-        char input[10];
-        inputFile.getline(input, 100);
-        cout << input << endl;
+        //cout << "Output " << i << " =" << tableData.combinations[i] << " " << tableData.indexMPN << endl;
+        i++;
     }
 
+    cout << "Table Row :\n\n";
+    for (int j = 0; j < 10; j++)
+    {
+        cout << "Row " << j + 1 << " = " << tableData.combinations[j] << ", " << tableData.indexMPN[j] << ", " << tableData.lower95[j] << ", " << tableData.upper95[j] << endl;
+    }
+
+    for (int j = 0; j < 10; j++)
+    {
+        cout << tableData.combinations[j] << ", ";
+    }
     //cout << "output = " << namespace_exit::x + namespace_insert::x;
     //cout << "find = " << yes.find('W') << endl;
     //string delet = yes.erase(0, 2 );
