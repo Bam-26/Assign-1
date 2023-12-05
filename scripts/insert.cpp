35a832d208ce812fe783036fa890d1980b541ca0
knshielim <151746945+knshielim@users.noreply.github.com>
2023-12-05 14:47:36 +0800

    Update insert.cpp

--- scripts/insert.cpp
+++ scripts/insert.cpp
@@ -1,11 +1,32 @@
 #include "tabledata.cpp"
 class Insert : public TableData
 {
-    public:
-    void insert()
+    string combinations;
+    int indexMPN;
+    int lower95;
+    int upper95;
+    void insert(string combination,int index, int lower95, int upper95)
     {
-        cout << "insert\n";
+
+        int n=11;
+        cout << "Enter the combination-of-positives triplet data: ";
+        cin >> tableData.combinations[n] << tableData.indexMPN[n] << tableData.lower95[n] << tableData.upper95[n];
+        for(n=0;n<10;n++)
+        {
+            cout << "Row " << n + 1 << " = " << tableData.combinations[n] << ", " << tableData.indexMPN[n] << ", " << tableData.lower95[n] << ", " << tableData.upper95[n] << endl;
+        }
+        while(tableData.combinations[n]<)
+    }
+    void rearrage()
+    {
+        ...
     }
 };
+
+int main()
+{
+        Insert myObj;
+        myObj.
+}
 
 Insert insert;
