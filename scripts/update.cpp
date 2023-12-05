d0bbd6bb27e43eb7e9480e24efce2d58588b5229
Noname <beatriceaurelia13@gmail.com>
2023-12-05 21:47:22 +0700

    Update update.cpp

--- scripts/update.cpp
+++ scripts/update.cpp
@@ -1,10 +1,16 @@
 #include "tabledata.cpp"
-class Update : public TableData
+class Update : public TableData, public search
 {
     public:
-    void update()
+    void update(int i)
     {
-        cout << "update\n";
+        search(int i);
+        if (h==1)
+        {
+            cout<<"Input the new number for MPN : ";            cin>>tableData.indexMPN[index];
+            cout<<"Input the new number for lower bounds:"      cin>>tableData.lower95[index];
+            cout<<"Input the new number for upper bounds:"      cin>>tableData.upper95[index];
+        }
     }
 };
 
