df9fd6c80c07ebb4ae33491fc3cabef4f4a625ca
QrJuicy <qrjuicy@gmail.com>
2023-12-06 19:59:49 +0800

    Fixed bugs

--- scripts/update.cpp
+++ scripts/update.cpp
@@ -1,15 +1,19 @@
 #include "tabledata.cpp"
-class Update : public TableData, public search
+#include "search.h"
+class Update : public TableData, public Search
 {
     public:
     void update(int i)
     {
-        search(int i);
+        appSearch(i);
         if (h==1)
         {
-            cout<<"Input the new number for MPN : ";            cin>>tableData.indexMPN[index];
-            cout<<"Input the new number for lower bounds:"      cin>>tableData.lower95[index];
-            cout<<"Input the new number for upper bounds:"      cin>>tableData.upper95[index];
+            cout<<"Input the new number for MPN : ";            
+            cin>>tableData.indexMPN[index];
+            cout << "Input the new number for lower bounds:";
+            cin >> tableData.lower95[index];
+            cout << "Input the new number for upper bounds:";
+            cin >> tableData.upper95[index];
         }
     }
 };
