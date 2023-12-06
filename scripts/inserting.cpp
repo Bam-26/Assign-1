40a97fb122dd7c1047631273ffd27ecdce5c5b7f
Noname <beatriceaurelia13@gmail.com>
2023-12-06 23:44:32 +0700

    Update inserting.cpp

--- scripts/inserting.cpp
+++ scripts/inserting.cpp
@@ -10,12 +10,16 @@ public:
         int y = tableData.lower95[i];
         int z = tableData.upper95[i];
         //set fixed variables for the inserted data
+
+
         int q=0;
         while(q<i && tableData.combinations[i]>tableData.combinations[q])
         {
             q++;
         }
         //Find position of inserted data in supposed order
+
+
         for(int j=i;j>q;--j)
         {
             tableData.combinations[j] = tableData.combinations[j - 1];
@@ -25,34 +29,47 @@ public:
         }
         //alternative code: sort(tableData.combinations, tableData.combinations + size);
         //rearrange elements in ascending order
+
+
         tableData.combinations[q] = w;
         tableData.indexMPN[q] = x;
         tableData.lower95[q] = y;
         tableData.upper95[q] = z;
         //set value of inserted combination
+
+
         for(int n=0;n<10;n++)
             {
                 cout << tableData.combinations[n] << "\t" << tableData.indexMPN[n] << "\t" << tableData.lower95[n] << "\t" << tableData.upper95[n] << endl;
             }
         //print out final table after insertion
-
     }
+
+
     void input(int &i)
     {
         string combi;
         cout << "No of combinations: " << i << endl;
         cout << "Enter the combination-of-positives triplet data: ";
         cin >> combi;
-        int k=0;
         //Inputing new combination-of-positive-triplet-data
+
+
+        int k=0;
         while(combi!=tableData.combinations[k] && k<i)
         {
             k++;
         }
+        //To find if there is a duplicate
+
+
         if(combi==tableData.combinations[k])
         {
             cout << "This combination is in the list." << endl;
         }
+        //Output when there is a duplicate
+
+
         if(combi!=tableData.combinations[k])
         {
             cout << "Enter the MPN-index data: ";
@@ -64,9 +81,9 @@ public:
             cout << "Enter the 95-percent-confidence-limit-upper data: ";
             cin >> tableData.upper95[i];
             //Inputing new 95-percent-confidence-limit-upper data
-
             tableData.combinations[i] = combi;
             rearrange(i);
+            //Call rearrange function
         }
     }
 
@@ -75,9 +92,6 @@ public:
 
         input(i);
         //call input function
-       // if(tableData.combinations[i]!=tableData.combinations[k])
-         //   rearrange(i);
-            //call rearrange function
     }
 };
 
