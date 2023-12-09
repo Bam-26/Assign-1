1245c104c95cb0b23660131dc4443d587130066a
QrJuicy <qrjuicy@gmail.com>
2023-12-09 18:23:06 +0800

    e

--- scripts/query.cpp
+++ scripts/query.cpp
@@ -8,7 +8,7 @@ private:
 
     string stringTemp1;
     string stringTemp2[216];
-    string stringTemp3;
+    string stringTemp3[216];
 
     string combination1;
     string combination2;
@@ -39,52 +39,82 @@ public:
         */
 
         cout << "Stack 1 :\n";
+        cout << "size = " << end(tableData.combinations) - begin(tableData.combinations) << endl;
         for (int n = 0; n < 17; n++)
         {
-            //cout << tableData.combinations[n].find('-') << endl;
             dashIndex = tableData.combinations[n].find('-');
             stringTemp1 = tableData.combinations[n].substr(0, dashIndex);
             if (stringTemp1 == combination1 || combination1 == "no")
             {
                 stack1[t] = n;
-                cout << tableData.combinations[n] << endl;
                 t++;
             }
-            //cout << tableData.combinations[n] << endl;
         }
-/*
+
+        
         for (int n = 0; n < 17; n++)
         {
-            cout << stack1[n] << endl;
-        }*/
+            cout << stack1[n]+1 << endl;
+        }
 
         stackIndex++;
         t = 0;
-        cout << stackIndex;
 
-        cout << "stack2 :\n";
-            for (int n = 0; n < 17; n++)
+
+        cout << endl << "stack2 :\n";
+        for (int n = 0; n < sizeof(stack1) / sizeof(int); n++)
         {
             tableIndex = stack1[n];
-            stringTemp2[t] = tableData.combinations[tableIndex];
+            stringTemp2[n] = tableData.combinations[tableIndex];
 
             for (int o = 0; o < stackIndex; o++)
             {
                 dashIndex = tableData.combinations[tableIndex].find("-");
-                stringTemp2[t].erase(0, dashIndex + 1);
+                stringTemp2[n].erase(0, dashIndex + 1);
             }
-            stringTemp2[t] = stringTemp2[t].substr(0, dashIndex);
-            if(stringTemp2[t] == combination2 || combination2 == "no")
+            stringTemp2[n] = stringTemp2[n].substr(0, dashIndex);
+            if(stringTemp2[n] == combination2 || combination2 == "no")
             {
-                cout << tableIndex << endl;
+                stack2[t] = tableIndex;
+                t++;
             }
-            //cout << stringTemp2[t] << endl;
-            t++;  
         }
-/*
-        //cout << combination1 << endl;
 
         
+        for (int n = 0; n < 17; n++)
+        {
+            cout << stack2[n]+1 << endl;
+        }
+        
+
+        stackIndex++;
+        t = 0;
+
+        cout << endl << "stack3 :\n";
+        for (int n = 0; n < sizeof(stack2) / sizeof(int); n++)
+        {
+            tableIndex = stack2[n];
+            stringTemp3[n] = tableData.combinations[tableIndex];
+
+            for (int o = 0; o < stackIndex; o++)
+            {
+                dashIndex = tableData.combinations[tableIndex].find("-");
+                stringTemp3[n].erase(0, dashIndex + 1);
+            }
+            stringTemp3[n] = stringTemp3[n].substr(0, dashIndex);
+            if (stringTemp3[n] == combination3 || combination3 == "no")
+            {
+                stack3[t] = tableIndex;
+                t++;
+            }
+        }
+
+        for (int n = 0; n < 3; n++)
+        {
+            cout << stack3[n]+1 << endl;
+        }
+
+/*
                  if (tableData.combinations[0][0] == combination1)
                  {
                      cout << "yassssssssss";
