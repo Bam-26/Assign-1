85d113b8cfc72b9ffd5fa128ed55fa5e08a5d90a
Noname <beatriceaurelia13@gmail.com>
2023-12-10 14:56:22 +0700

    Update search.cpp

--- scripts/search.cpp
+++ scripts/search.cpp
@@ -5,24 +5,24 @@ class Search : public TableData
         string search_combination;
         int h;
         int index;
-    void input_search()
+    void input_search() // to input the combination-of-positive-triplet-data
     {
         cout<<"Input combination of positives: ";
         cin>>search_combination;
     }
     int appSearch(int i)
     {
-        input_search();
+        input_search(); // call the function for combination input
         for (int n=0; n<i; n++)
         {
-            if (search_combination==tableData.combinations[n])
+            if (search_combination==tableData.combinations[n]) // Looking for the same combination in the list as the input combination
             {
                 cout<<"For "<<tableData.combinations[n];
                 cout<<", MPN = "<<tableData.indexMPN[n]<<";";
                 cout<<" 95% of samples contain between "<<tableData.lower95[n];
                 cout<<" and "<<tableData.upper95[n]<<" bacterial/ml."<<endl;
-                h=1;
-                index=n;
+                h=1; // as an index if the combination sought is in the list
+                index=n; // for the update menu where the data in this combination will be updated
             }
         }
         if (h==0)
